e03008d61677c3b94e51480c904ac2a6c405762a
Daesung.lee <daesung.lee@naver.com>
2021-04-27 13:30:43 +0900

    주소 파라미터 값으로 테마 및 라이트 페이지 이동시에도 값 유지 하기.

--- js/front-ui.js
+++ js/front-ui.js
@@ -2,6 +2,7 @@ var patOnTheBackPop = false;
 var UI = {
     init : function(){
         this.loaderAdd();
+        this.checkTheme();
         this.resizeCheckReset();
         this.lampDraw();
         this.sectionToggleSwitchEvent();
@@ -10,6 +11,7 @@ var UI = {
         this.mainVisualSwitchEvent();
         this.visualMouseClickEvent();
         this.gnbListDraw();
+        this.gnbLinkThemeCheckEvent();
         this.snsShare();
         this.InterestListDraw(InterestList)
 
@@ -26,6 +28,149 @@ var UI = {
         if($("[data-dropdown]").length > 0){this.dropdown()}
         if($("[data-slick]").length > 0){this.techListDraw(techLists);}
         if($("[data-toggle]").length > 0){this.toggleClassDefault;}
+    },
+    searchParam: function (key) {
+        return new URLSearchParams(location.search).get(key);
+    },
+    checkTheme : function () {
+        var $theme = this.searchParam("theme");
+        var $sectionLight = this.searchParam("section");
+        var $parameter = location.href;
+
+        console.log($theme, $sectionLight)
+
+        if($theme === "on"){
+            var $toggleBtn = $('.visual-toggle');
+            var $visual = $('.visual-spot');
+            var $mouse = $('.mouse');
+            var $theme = $("body");
+
+            $toggleBtn.addClass('is-on');
+            $visual.addClass('is-on');
+            $theme.addClass('theme');
+            checkTheme = true;
+
+            (function() {
+                var lastTime = 0;
+                var vendors = ['ms', 'moz', 'webkit', 'o'];
+                for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+                    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+                    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+                        || window[vendors[x] + 'CancelRequestAnimationFrame'];
+                }
+
+                if (!window.requestAnimationFrame)
+                    window.requestAnimationFrame = function (callback, element) {
+                        var currTime = new Date().getTime();
+                        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                        var id = window.setTimeout(function () {
+                                callback(currTime + timeToCall);
+                            },
+                            timeToCall);
+                        lastTime = currTime + timeToCall;
+
+                        return id;
+                    };
+
+                if (!window.cancelAnimationFrame)
+                    window.cancelAnimationFrame = function (id) {clearTimeout(id);};
+            }());
+
+            (function() {
+                var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
+
+                // Main
+                initHeader();
+                addListeners();
+
+                function initHeader() {
+                    width = document.querySelector(".spot-inner").offsetWidth;
+                    height = document.querySelector(".spot-inner").offsetHeight;
+                    target = { x: 0, y: height };
+
+                    largeHeader = document.getElementById("visual-spot");
+                    largeHeader.style.height = height + 'px';
+
+                    canvas = document.getElementById('water-bell-canvas');
+                    canvas.style.display = "block";
+                    canvas.width = width;
+                    canvas.height = height;
+                    ctx = canvas.getContext('2d');
+
+                    // create particles
+                    circles = [];
+                    for (var x = 0; x < width * 0.5; x++) {
+                        var c = new Circle();
+                        circles.push(c);
+                    }
+                    animate();
+                }
+
+                // Event handling
+                function addListeners() {
+                    window.addEventListener('scroll', scrollCheck);
+                    window.addEventListener('resize', resize);
+                }
+
+                function scrollCheck() {
+                    if (document.body.scrollTop > height) animateHeader = false;
+                    else animateHeader = true;
+                }
+
+                function resize() {
+                    width = document.querySelector(".spot-inner").offsetWidth;
+                    height = document.querySelector(".spot-inner").offsetHeight;
+                    largeHeader.style.height = height + 'px';
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+
+                function animate() {
+                    if (animateHeader) {
+                        ctx.clearRect(0, 0, width, height);
+                        for (var i in circles) {
+                            circles[i].draw();
+                        }
+                    }
+                    requestAnimationFrame(animate);
+                }
+
+                // Canvas manipulation
+                function Circle() {
+                    var _this = this;
+
+                    // constructor
+                    (function () {
+                        _this.pos = {};
+                        init();
+                    })();
+
+                    function init() {
+                        _this.pos.x = Math.random() * width;
+                        _this.pos.y = height + Math.random() * 100;
+                        _this.alpha = 0.1 + Math.random() * 0.3;
+                        _this.scale = 0.3 + Math.random() * 0.3;
+                        _this.velocity = Math.random();
+                    }
+
+                    this.draw = function () {
+                        if (_this.alpha <= 0) {
+                            init();
+                        }
+                        _this.pos.y -= _this.velocity;
+                        _this.alpha -= 0.0005;
+                        ctx.beginPath();
+                        ctx.arc(_this.pos.x, _this.pos.y, _this.scale * 10, 0, 2 * Math.PI, false);
+                        ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
+                        ctx.fill();
+                    };
+                }
+            }())
+        }
+        if($sectionLight === "on"){
+            $(".content > .section").addClass("is-on")
+            checkLight = true
+        }
     },
     techListDraw : function( data ){
         $.each(data, function (i) {
@@ -242,7 +387,34 @@ var UI = {
         var $array = gnbList;
 
         $.each($array, function (key, value) {
-            $el.append('<li><a href="' + value.url + '">' + value.name + '</a></li>');
+            $el.append('<li><a href="javascript:void(0)" data-href="' + value.url + '">' + value.name + '</a></li>');
+        });
+    },
+    gnbLinkThemeCheckEvent : function () {
+        var gnbGroup = [];
+
+        $("[data-href]").each(function () {
+            if($.inArray($(this).data("href"),gnbGroup ) === -1) {
+                gnbGroup.push($(this).data("href"));
+            }
+        })
+
+        console.log(gnbGroup);
+        $.each(gnbGroup, function (key, value){
+            var $body = $("body");
+            var $btn = $("[data-href='" + value + "']");
+
+            $btn.on("click", function () {
+                if(checkTheme) {
+                    if (checkLight){
+                        $(this).attr("href", value + "?theme=on&section=on")
+                    } else {
+                        $(this).attr("href", value + "?theme=on")
+                    }
+                } else {
+                    $(this).attr("href", value)
+                }
+            })
         });
     },
     toggleClassDefault : function () {
@@ -515,7 +687,13 @@ var UI = {
     sectionToggleSwitchEvent : function(){
         var $btn = $(".section-inner .toggle-switch");
         $btn.on('click', function(){
-            $(this).parents('.section').toggleClass("is-on");
+            if(checkLight !== true){
+                $(this).parents('.section').addClass("is-on")
+                checkLight = true
+            } else {
+                $(this).parents('.section').removeClass("is-on")
+                checkLight = false
+            }
         });
     },
     visualMouseClickEvent : function () {
@@ -884,16 +1062,13 @@ var UI = {
 };
 
 var checkTheme = false;
+var checkLight = false;
 var exhibitionTitle = $("html").find("title").text(); // 공유 하는 페이지 제목
 
 //ready
 $(function(){
 
     UI.init();
-
-    if($themeOncheck){
-        $('.visual-toggle').trigger("click");
-    }
 
 });
 
