43677475204f9a7f67a09624e6b43974e57599fe
daesung.lee <leeds019@naver.com>
2021-11-08 14:45:56 +0900

    데이터 경로 수정

--- js/control-data.js
+++ js/control-data.js
@@ -1,13 +1,13 @@
-$.getScript( "./js/data-gnb.js");
-$.getScript( "./js/data-experience.js");
-$.getScript( "./js/data-encouragement.js");
-$.getScript( "./js/data-history.js");
-$.getScript( "./js/data-util.js");
-$.getScript( "./js/data-wish.js");
-$.getScript( "./js/data-roadmap.js");
-$.getScript( "./js/data-profile.js");
-$.getScript( "./js/data-itWordList.js");
-$.getScript( "./js/data-web-accessibility.js");
+$.getScript( "js/data-gnb.js");
+$.getScript( "js/data-experience.js");
+$.getScript( "js/data-encouragement.js");
+$.getScript( "js/data-history.js");
+$.getScript( "js/data-util.js");
+$.getScript( "js/data-wish.js");
+$.getScript( "js/data-roadmap.js");
+$.getScript( "js/data-profile.js");
+$.getScript( "js/data-itWordList.js");
+$.getScript( "js/data-web-accessibility.js");
 
 /*document.write("<script src='./js/data-gnb.js'></script>");
 document.write("<script src='./js/data-experience.js'></script>");
