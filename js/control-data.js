3420a102613a6ccd2bff4101f20aa90661e9c965
daesung.lee <leeds019@naver.com>
2021-11-08 15:30:38 +0900

    데이터 경로 수정

--- js/control-data.js
+++ js/control-data.js
@@ -1,13 +1,33 @@
-$.getScript( "js/data-gnb.js");
-$.getScript( "js/data-experience.js");
-$.getScript( "js/data-encouragement.js");
-$.getScript( "js/data-history.js");
-$.getScript( "js/data-util.js");
-$.getScript( "js/data-wish.js");
-$.getScript( "js/data-roadmap.js");
-$.getScript( "js/data-profile.js");
-$.getScript( "js/data-itWordList.js");
-$.getScript( "js/data-web-accessibility.js");
+$.getScript( "js/data-gnb.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-experience.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-encouragement.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-history.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-util.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-wish.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-roadmap.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-profile.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-itWordList.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
+$.getScript( "js/data-web-accessibility.js",function(data, dataStatus,jqxhr){
+    console.log(data, dataStatus, jqxhr.status)
+});
 
 
 // var $themeOncheck = false;
