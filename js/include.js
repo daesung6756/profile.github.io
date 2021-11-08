981ef7d83dd2267b0ebeb1ee9af05637c916e58c
daesung.lee <leeds019@naver.com>
2021-11-08 15:52:57 +0900

    데이터 경로 변경

--- js/include.js
+++ js/include.js
@@ -30,7 +30,19 @@ var include = {
             document.write('<script src="js/vendor/jquery-3.4.0.min.js"></script>');
             document.write('<script src="js/vendor/p5.min.js"></script>');
             document.write('<script src="js/grm.js"></script>');
-            document.write('<script src="js/control-data.js"></script>');
+
+            document.write('<script src="js/data-gnb.js"></script>');
+            document.write('<script src="js/data-experience.js"></script>');
+            document.write('<script src="js/data-encouragement.js"></script>');
+            document.write('<script src="js/data-history.js"></script>');
+            document.write('<script src="js/data-util.js"></script>');
+            document.write('<script src="js/data-wish.js"></script>');
+            document.write('<script src="js/data-roadmap.js"></script>');
+            document.write('<script src="js/data-profile.js"></script>');
+            document.write('<script src="js/data-itWordList.js"></script>');
+
+
+            /*document.write('<script src="js/control-data.js"></script>');*/
 
         }
     },
