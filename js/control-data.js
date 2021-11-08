71363071899c53d258ee40cca725f280fc5d9844
daesung.lee <leeds019@naver.com>
2021-11-08 15:12:06 +0900

    데이터 경로 수정

--- js/control-data.js
+++ js/control-data.js
@@ -1,32 +1,22 @@
-$.getScript( "js/data-gnb.js", function() {
-    alert("data-gnb.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-experience.js", function() {
-    alert("data-experience.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-encouragement.js", function() {
-    alert("data-encouragement.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-history.js", function() {
-    alert("data-history.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-util.js", function() {
-    alert("data-util.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-wish.js", function() {
-    alert("data-wish.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-roadmap.js", function() {
-    alert("data-roadmap.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-profile.js", function() {
-    alert("data-profile.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-itWordList.js", function() {
-    alert("data-itWordList.js 데이터 불러 왔습니다.")
-});
-$.getScript( "js/data-web-accessibility.js", function() {
-    alert("data-web-accessibility.js 데이터 불러 왔습니다.")
-});
+$.getScript( "js/data-gnb.js");
+$.getScript( "js/data-experience.js");
+$.getScript( "js/data-encouragement.js");
+$.getScript( "js/data-history.js");
+$.getScript( "js/data-util.js");
+$.getScript( "js/data-wish.js");
+$.getScript( "js/data-roadmap.js");
+$.getScript( "js/data-profile.js");
+$.getScript( "js/data-itWordList.js");
+$.getScript( "js/data-web-accessibility.js");
+
+/*document.write("<script src='./js/data-gnb.js'></script>");
+document.write("<script src='./js/data-experience.js'></script>");
+document.write("<script src='./js/data-encouragement.js'></script>");
+document.write("<script src='./js/data-history.js'></script>");
+document.write("<script src='./js/data-util.js'></script>");
+document.write("<script src='./js/data-wish.js'></script>");
+document.write("<script src='./js/data-roadmap.js'></script>");
+document.write("<script src='./js/data-profile.js'></script>");
+document.write("<script src='./js/data-itWordList.js'></script>");*/
 
 var $themeOncheck = false;
